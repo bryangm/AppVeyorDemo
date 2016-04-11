da1c2bea8c5757d73eafa6040f16ca3fb091d2c3
Bryan Mack <bmack@ons.org>
2016-04-10 21:59:29 -0400

    added values controller

---
+++ src/ValuesApi/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ValuesApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("ping")]
+        public IHttpActionResult Ping()
+        {
+            return this.Ok("Pong.");
+        }
+    }
+}
